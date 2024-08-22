90ba948260fdd895d5cbfe6b9080cab042174f16
solafeng2024 <solafeng@outlook.com>
2024-08-22 14:22:09 +0800

    update the style and upload the 20240822 review

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
-import { defineConfig } from 'astro/config'
-import mdx from '@astrojs/mdx'
-import react from '@astrojs/react'
+import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
+import react from '@astrojs/react';
+import sitemap from '@astrojs/sitemap';
 
-import sitemap from '@astrojs/sitemap'
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://solafeng2024.github.io',
-  integrations: [mdx(), sitemap(), react()],
-})
+  integrations: [mdx(), sitemap(), react(), tailwind()]
+});
